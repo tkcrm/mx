83acdc4f5d6092d7c1f70d99e88bbd1c6dcd9f1a
sxwebdev <sxwebdev@gmail.com>
2024-07-24 06:19:28 -0700

    update debugger

--- ops/profiler.go
+++ ops/profiler.go
@@ -3,6 +3,8 @@ package ops
 import (
 	"net/http"
 	"net/http/pprof"
+
+	"github.com/tkcrm/mx/transport/http_transport"
 )
 
 type profilerOpsService struct {
@@ -14,9 +16,10 @@ func newProfilerOpsService(cfg ProfilerConfig) *profilerOpsService {
 }
 
 type ProfilerConfig struct {
-	Enabled bool   `default:"false" usage:"allows to enable profiler" example:"false"`
-	Path    string `default:"/debug/pprof" validate:"required" usage:"allows to set custom profiler path" example:"/debug/pprof"`
-	Port    string `default:"10000" validate:"required" usage:"allows to set custom profiler port" example:"10000"`
+	Enabled      bool   `default:"false" usage:"allows to enable profiler" example:"false"`
+	Path         string `default:"/debug/pprof" validate:"required" usage:"allows to set custom profiler path" example:"/debug/pprof"`
+	Port         string `default:"10000" validate:"required" usage:"allows to set custom profiler port" example:"10000"`
+	WriteTimeout int    `yaml:"write_timeout" default:"60" usage:"HTTP server write timeout in seconds" example:"60"`
 }
 
 func (s profilerOpsService) Name() string { return "profiler" }
@@ -31,6 +34,16 @@ func (s profilerOpsService) initService(mux *http.ServeMux) {
 	mux.HandleFunc(s.config.Path+"/profile", pprof.Profile)
 	mux.HandleFunc(s.config.Path+"/symbol", pprof.Symbol)
 	mux.HandleFunc(s.config.Path+"/trace", pprof.Trace)
+}
+
+func (s profilerOpsService) getHttpOptions() []http_transport.Option {
+	res := make([]http_transport.Option, 0)
+
+	if s.config.WriteTimeout > 0 {
+		res = append(res, http_transport.WithWriteTimeout(s.config.WriteTimeout))
+	}
+
+	return res
 }
 
 var _ opsService = (*profilerOpsService)(nil)
--- transport/grpc_transport/go.mod
+++ transport/grpc_transport/go.mod
@@ -20,6 +20,6 @@ require (
 	golang.org/x/net v0.27.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240709173604-40e1e62336c5 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240723171418-e6d459c13d2a // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 )
