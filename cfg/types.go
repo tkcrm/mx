2c3720ade70f9e8ae4163a2d83afd5fd52149054
sxwebdev <sxwebdev@gmail.com>
2024-09-20 15:01:02 +0300

    cfg add GetConfigLoader

--- cfg/types.go
+++ cfg/types.go
@@ -1,13 +1,13 @@
 package cfg
 
-type configField struct {
-	path              string
-	envName           string
-	defaultValue      string
-	usage             string
-	example           string
-	validateParams    string
-	isRequired        bool
-	isSecret          bool
-	disableValidation bool
+type ConfigField struct {
+	Path              string
+	EnvName           string
+	DefaultValue      string
+	Usage             string
+	Example           string
+	ValidateParams    string
+	IsRequired        bool
+	IsSecret          bool
+	DisableValidation bool
 }
